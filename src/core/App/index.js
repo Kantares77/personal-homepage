5ba6cc602da7046fc2b206398435cb556c3c3aea
Kantares77 <marcin.kantarski77@gmail.com>
2025-11-13 20:49:12 +0100

    App. js added with proper configuration and imported of  necessary filies

---
+++ src/core/App/index.js
@@ -0,0 +1,21 @@
+import { useSelector } from "react-redux";
+import { selectIsDarkTheme } from "../../common/themeSlice";
+import { ThemeProvider } from "styled-components";
+import { themeDark, themeLight } from "./theme";
+import { Normalize } from "styled-normalize";
+import { GlobalStyle } from "./GlobalStyle";
+import { PersonalHomepage } from "../../features/PersonalHomepage";
+
+const App = () => {
+    const isDarkTheme = useSelector(selectIsDarkTheme);
+
+    return (
+        <ThemeProvider theme={isDarkTheme ? themeDark : themeLight}>
+            <Normalize />
+            <GlobalStyle />
+            <PersonalHomepage />
+        </ThemeProvider>
+    );
+};
+
+export default App;
