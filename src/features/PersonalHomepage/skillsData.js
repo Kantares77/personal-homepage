66b91e241bdf706fd91f22cecf865946e25d47d3
Kantares77 <marcin.kantarski77@gmail.com>
2025-11-23 19:13:28 +0100

    created a separate file with list of my current and next skills

---
+++ src/features/PersonalHomepage/skillsData.js
@@ -0,0 +1,29 @@
+export const currentSkills = [
+    { id: 1, skill: "HTML" },
+    { id: 2, skill: "CSS Flexbox" },    
+    { id: 3, skill: "Error handling" },
+    { id: 4, skill: "Responsive Web Design" },
+    { id: 5, skill: "CSS Grid" },
+    { id: 6, skill: "Promises, Async/Await" },
+    { id: 7, skill: "Markdown" },
+    { id: 8, skill: "Redux (Toolkit)" },
+    { id: 9, skill: "Working with API (fetch, axios)" },
+    { id: 10, skill: "Teamwork" },
+    { id: 11, skill: "Redux-Saga" },
+    { id: 12, skill: "Git" },
+    { id: 13, skill: "Immutability" },
+    { id: 14, skill: "React" },
+    { id: 15, skill: "NPM" },
+    { id: 16, skill: "JavaScript" },
+    { id: 17, skill: "React Hooks" },
+    { id: 18, skill: "Scrum" },
+    { id: 19, skill: "GitHub Pull Requests & Review" },
+    { id: 20, skill: "React Router" },
+    { id: 21, skill: "Trello" },
+    { id: 22, skill: "CSS BEM convention" },
+];
+
+export const nextSkills = [
+    { id: 1, skill: "TypeScript" },
+    { id: 2, skill: "Next.js" },
+];
