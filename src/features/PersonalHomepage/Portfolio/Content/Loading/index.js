ea5b30cdaa9a0fb3871f799dc449384b70b2f73b
Kantares77 <marcin.kantarski77@gmail.com>
2025-11-23 19:09:30 +0100

    "Loading" component added

---
+++ src/features/PersonalHomepage/Portfolio/Content/Loading/index.js
@@ -0,0 +1,8 @@
+import { StyledLoading, Wrapper } from "./styled";
+
+export const Loading = () => (
+    <Wrapper>
+        Please wait, projects are&nbsp;being&nbsp;loaded...
+        <StyledLoading />
+    </Wrapper>
+);
